6f0b883a187607219d12b7a3dfc90132a0049cea
alan-m-a <alan.m.alegre@gmail.com>
2021-10-07 15:04:00 -0300

    NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../../images/logo.svg';
+
+
+export const Navbar = () => {
+    return (
+        <>
+            <nav className='myNav'>
+            <img src={logo} className='myLogo' alt='logo' />
+                <ul>
+                    <a  href='index.html'>Home</a>
+                    <a  href='index.html'>Servicios</a>
+                    <a  href='index.html'>Contacto</a>
+                    <a  href='index.html'>Modelos Soportados</a>
+                    <a  href='index.html'>Registro</a>
+                    <a  href='index.html'>Login</a>
+                </ul>
+            </nav>
+        </>
+    )
+}
